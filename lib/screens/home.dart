584e12b3e56dfc850af2b0eb018410020aec15ab
Aditi <adithakurti@gmail.com>
2023-05-16 21:25:39 +0530

    Error solved Added null variable

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:currency_convertor/functions/fetchrates.dart';
 import 'package:currency_convertor/models/ratesmodel.dart';
 import 'package:flutter/material.dart';
 
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -42,28 +43,21 @@ class _HomeState extends State<Home> {
         ),
         child: SingleChildScrollView(
           child: Form(key: formkey,
-            child: FutureBuilder<RatesModel>(
-              future: result,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (snapshot.hasData) {
-                  // Perform null check on snapshot.data
-                  final rates = snapshot.data!.rates;
-                  return Center(
-                    child: Text(rates.toString()),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error: ${snapshot.error}'),
-                  );
-                }
-                return Container(); // Return an appropriate fallback widget if necessary
-              },
-            ),
+          child: FutureBuilder<RatesModel>(
+            future: result,
+            builder: (context,snapshot){
+              if(
+              snapshot.connectionState == ConnectionState.waiting
+              ){
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Center(
+                child: Text(snapshot.data!.rates.toString(),style: TextStyle(color: Colors.white,fontSize: 20,),),
+              );
+            },
+          ),
           ),
         ),
       ),
